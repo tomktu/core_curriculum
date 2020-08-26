616be2de25003407fb6abe50bc00ad8c9677311d
Tom Tu <tomktu@gmail.com>
2020-08-25 23:29:06 -0400

    changes to code based on recommendations from rubocop

--- rb101/L2_smallprograms/calculator.rb
+++ rb101/L2_smallprograms/calculator.rb
@@ -85,22 +85,21 @@ loop do # main loop
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-            when '1'
-              number1.to_i() + number2.to_i()
-            when '2'
-              number1.to_i() - number2.to_i()
-            when '3'
-              number1.to_i() * number2.to_i()
-            when '4'
-              number1.to_f() / number2.to_f()
-          end
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+           end
 
   prompt("The result is #{result}")
 
   prompt("Do you want to perform another calculation? (Y to calculate again)")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
-
 end
 
 prompt("Thank you for using the calculator. Good bye!")
