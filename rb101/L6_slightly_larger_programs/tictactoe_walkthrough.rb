4562314100d9be827518bfd60b662b391f3fe891
Tom Tu <28871393+tomktu@users.noreply.github.com>
2021-02-13 14:00:02 -0500

    rubocop edits

--- rb101/L6_slightly_larger_programs/tictactoe_walkthrough.rb
+++ rb101/L6_slightly_larger_programs/tictactoe_walkthrough.rb
@@ -3,11 +3,15 @@ require 'pry'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                [[1, 5, 9], [3, 5, 7]]
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop: disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -25,15 +29,16 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop: enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -63,18 +68,19 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] +
-                  [[1,4,7], [2,5,8], [3,6,9]] +
-                  [[1,5,9], [3,5,7]]
-
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #    brd[line[1]] == PLAYER_MARKER &&
+    #    brd[line[2]] == PLAYER_MARKER
+    #   return 'Player'
+    # elsif brd[line[0]] == COMPUTER_MARKER &&
+    #       brd[line[1]] == COMPUTER_MARKER &&
+    #       brd[line[2]] == COMPUTER_MARKER
+    #   return 'Computer'
+    # end
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
