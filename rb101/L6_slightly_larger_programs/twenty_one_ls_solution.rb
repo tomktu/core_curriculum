eaf0dddc881f292bf2428e01543c6ec494766620
Tom Tu <28871393+tomktu@users.noreply.github.com>
2021-02-25 11:04:32 -0500

    added bonus features

--- rb101/L6_slightly_larger_programs/twenty_one_ls_solution.rb
+++ rb101/L6_slightly_larger_programs/twenty_one_ls_solution.rb
@@ -1,5 +1,7 @@
 SUITS = ['H', 'D', 'S', 'C']
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+WHATEVER_ONE = 21
+DEALER_HIT_THRESHOLD = 17
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -15,32 +17,32 @@ def total(cards)
 
   sum = 0
   values.each do |value|
-    if value == "A"
-      sum += 11
-    elsif value.to_i == 0 # J, Q, K
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value == "A"
+             11
+           elsif value.to_i == 0 # J, Q, K
+             10
+           else
+             value.to_i
+           end
   end
 
   # correct for Aces
   values.select { |value| value == "A" }.count.times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > WHATEVER_ONE
   end
 
   sum
 end
 
 def busted?(total)
-  total > 21
+  total > WHATEVER_ONE
 end
 
 # :tie, :dealer, :player, :dealer_busted, :player_busted
 def detect_result(dealer_total, player_total)
-  if player_total > 21
+  if player_total > WHATEVER_ONE
     :player_busted
-  elsif dealer_total > 21
+  elsif dealer_total > WHATEVER_ONE
     :dealer_busted
   elsif dealer_total < player_total
     :player
@@ -76,7 +78,7 @@ def play_again?
 end
 
 def display_cards_and_total(dealer_cards, dealer_total, player_cards,
-  player_total)
+                            player_total)
   puts "=============="
   prompt "Dealer has #{dealer_cards}, for a total of: #{dealer_total}"
   prompt "Player has #{player_cards}, for a total of: #{player_total}"
@@ -87,84 +89,123 @@ def clear_screen
   system('clear') || system('cls')
 end
 
+def increment_score(score, player)
+  score[player] += 1
+end
+
 loop do
   clear_screen
-  prompt "Welcome to Twenty-One!"
+  prompt "Welcome to #{WHATEVER_ONE}!"
+  score = { dealer: 0, player: 0 }
 
-  # initialize vars
-  deck = initialize_deck
-  player_cards = []
-  dealer_cards = []
-
-  # initial deal
-  2.times do
-    player_cards << deck.pop
-    dealer_cards << deck.pop
-  end
-
-  player_total = total(player_cards)
-  dealer_total = total(dealer_cards)
-
-  prompt "Dealer has #{dealer_cards[0]} and ?"
-  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}."
-
-  # player turn
   loop do
-    player_turn = nil
-    loop do
-      prompt "Would you like to (h)it or (s)tay?"
-      player_turn = gets.chomp.downcase
-      break if ['h', 's'].include?(player_turn)
-      prompt "Sorry, must enter 'h' or 's'."
-    end
+    # clear_screen
+    prompt "Current score is: You - #{score[:player]}, Dealer -"\
+    " #{score[:dealer]}"
 
-    if player_turn == 'h'
+    # initialize vars
+    deck = initialize_deck
+    player_cards = []
+    dealer_cards = []
+
+    # initial deal
+    2.times do
       player_cards << deck.pop
-      player_total = total(player_cards)
-
-      prompt "You chose to hit!"
-      prompt "Your cards are now: #{player_cards}"
-      prompt "Your total is now: #{player_total}"
+      dealer_cards << deck.pop
     end
 
-    break if player_turn == 's' || busted?(player_total)
-  end
-
-  if busted?(player_total)
-    display_cards_and_total(dealer_cards, dealer_total, player_cards,
-      player_total)
-    display_result(dealer_total, player_total)
-    play_again? ? next : break
-  else
-    prompt "You stayed at #{player_total}"
-  end
-
-  # dealer turn
-  prompt "Dealer turn..."
-
-  loop do
-    break if dealer_total >= 17
-
-    prompt "Dealer hits!"
-    dealer_cards << deck.pop
-    prompt "Dealer's cards are now: #{dealer_cards}"
+    player_total = total(player_cards)
     dealer_total = total(dealer_cards)
-  end
 
-  if busted?(dealer_total)
-    prompt "Dealer total is now: #{dealer_total}"
+    prompt "Dealer has #{dealer_cards[0]} and ?"
+    prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total"\
+    " of #{player_total}."
+
+    # player turn
+    loop do
+      player_turn = nil
+      loop do
+        prompt "Would you like to (h)it or (s)tay?"
+        player_turn = gets.chomp.downcase
+        break if ['h', 's'].include?(player_turn)
+        prompt "Sorry, must enter 'h' or 's'."
+      end
+
+      if player_turn == 'h'
+        player_cards << deck.pop
+        player_total = total(player_cards)
+
+        prompt "You chose to hit!"
+        prompt "Your cards are now: #{player_cards}"
+        prompt "Your total is now: #{player_total}"
+      end
+
+      break if player_turn == 's' || busted?(player_total)
+    end
+
+    if busted?(player_total)
+      increment_score(score, :dealer)
+      display_cards_and_total(dealer_cards, dealer_total, player_cards,
+                              player_total)
+      display_result(dealer_total, player_total)
+
+      sleep(5)
+      clear_screen
+      next
+    else
+      prompt "You stayed at #{player_total}"
+    end
+
+    # dealer turn
+    prompt "Dealer turn..."
+
+    loop do
+      break if dealer_total >= DEALER_HIT_THRESHOLD
+
+      prompt "Dealer hits!"
+      dealer_cards << deck.pop
+      prompt "Dealer's cards are now: #{dealer_cards}"
+      dealer_total = total(dealer_cards)
+    end
+
+    if busted?(dealer_total)
+      prompt "Dealer total is now: #{dealer_total}"
+      increment_score(score, :player)
+      display_cards_and_total(dealer_cards, dealer_total, player_cards,
+                              player_total)
+      display_result(dealer_total, player_total)
+
+      sleep(5)
+      clear_screen
+      next
+    else
+      prompt "Dealer stays at #{dealer_total}"
+    end
+
+    # both player and dealer stays - compare cards!
     display_cards_and_total(dealer_cards, dealer_total, player_cards,
-      player_total)
+                            player_total)
     display_result(dealer_total, player_total)
-    play_again? ? next : break
-  else
-    prompt "Dealer stays at #{dealer_total}"
+
+    winner = detect_result(dealer_total, player_total)
+
+    case winner
+    when :player
+      increment_score(score, :player)
+    when :dealer
+      increment_score(score, :dealer)
+    end
+
+    sleep(5)
+    break if score[:dealer] == 5 || score[:player] == 5
+    clear_screen
   end
 
-  # both player and dealer stays - compare cards!
-  display_cards_and_total(dealer_cards, dealer_total, player_cards,
-    player_total)
-  display_result(dealer_total, player_total)
+  if score[:player] == 5
+    prompt "Congrats, you are the ultimate winner!"
+  else
+    prompt "Dealer is the first to 5 wins. Better luck next time."
+  end
 
   break unless play_again?
 end
